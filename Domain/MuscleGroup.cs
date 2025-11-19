b04e46b3a3571418ff487e18d2d9e9adcddcffa7
ITIvanov18 <59703243+ITIvanov18@users.noreply.github.com>
2025-11-19 23:34:07 +0200

    Add basic enums: MuscleGroup and WorkoutStatus

---
+++ Domain/MuscleGroup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gym_Workout_Diary___Tracker.Domain
+{
+    // ------------------------------------------------------------
+    // Student: Ivan Tenev Ivanov
+    // Faculty Number: F115436
+    // File: MuscleGroup.cs
+    // ------------------------------------------------------------
+
+    /// <summary>
+    /// Enum тип за основните мускулни групи или видове тренировки
+    /// </summary>
+    public enum MuscleGroup
+    {
+        FullBody,
+        Push,
+        Pull,
+        Legs,
+        Upper,
+        Lower,
+        Cardio,
+        Other
+    }
+}
---
+++ Domain/WorkoutStatus.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gym_Workout_Diary___Tracker.Domain
+{
+    // ------------------------------------------------------------
+    // Student: Ivan Tenev Ivanov
+    // Faculty Number: F115436
+    // File: WorkoutStatus.cs
+    // ------------------------------------------------------------
+
+    /// <summary>
+    /// Enum тип за статус на тренировъчен запис в дневника
+    /// </summary>
+    public enum WorkoutStatus
+    {
+        /// <summary>
+        /// Тренировката е планирана, но още не е изпълнена
+        /// </summary>
+        Planned,
+
+        /// <summary>
+        /// Тренировката е изпълнена и записът е завършен
+        /// </summary>
+        Completed
+    }
+}
