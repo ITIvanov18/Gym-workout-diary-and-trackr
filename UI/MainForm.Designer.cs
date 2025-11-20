144de4aa776c579a9a50f769232a28a0fb8e188e
ITIvanov18 <59703243+ITIvanov18@users.noreply.github.com>
2025-11-20 02:33:41 +0200

    Add initial UI layer with WinForms layout and event handlers

---
+++ UI/MainForm.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Gym_Workout_Diary___Tracker.UI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxEntries = new System.Windows.Forms.ListBox();
+            this.listBoxExercises = new System.Windows.Forms.ListBox();
+            this.buttonAddEntry = new System.Windows.Forms.Button();
+            this.buttonRemoveEntry = new System.Windows.Forms.Button();
+            this.buttonAddExercise = new System.Windows.Forms.Button();
+            this.btnRemoveExercise = new System.Windows.Forms.Button();
+            this.buttonMarkCompleted = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.labelOverallProgress = new System.Windows.Forms.Label();
+            this.labelVolumeCaption = new System.Windows.Forms.Label();
+            this.labelVolumeValue = new System.Windows.Forms.Label();
+            this.buttonShowStats = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxEntries
+            // 
+            this.listBoxEntries.FormattingEnabled = true;
+            this.listBoxEntries.ItemHeight = 20;
+            this.listBoxEntries.Location = new System.Drawing.Point(10, 12);
+            this.listBoxEntries.Name = "listBoxEntries";
+            this.listBoxEntries.Size = new System.Drawing.Size(333, 384);
+            this.listBoxEntries.TabIndex = 0;
+            this.listBoxEntries.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBoxExercises
+            // 
+            this.listBoxExercises.FormattingEnabled = true;
+            this.listBoxExercises.ItemHeight = 20;
+            this.listBoxExercises.Location = new System.Drawing.Point(367, 12);
+            this.listBoxExercises.Name = "listBoxExercises";
+            this.listBoxExercises.Size = new System.Drawing.Size(450, 384);
+            this.listBoxExercises.TabIndex = 1;
+            this.listBoxExercises.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged_1);
+            // 
+            // buttonAddEntry
+            // 
+            this.buttonAddEntry.Location = new System.Drawing.Point(10, 430);
+            this.buttonAddEntry.Name = "buttonAddEntry";
+            this.buttonAddEntry.Size = new System.Drawing.Size(165, 39);
+            this.buttonAddEntry.TabIndex = 2;
+            this.buttonAddEntry.Text = "Add sample day";
+            this.buttonAddEntry.UseVisualStyleBackColor = true;
+            // 
+            // buttonRemoveEntry
+            // 
+            this.buttonRemoveEntry.Location = new System.Drawing.Point(195, 430);
+            this.buttonRemoveEntry.Name = "buttonRemoveEntry";
+            this.buttonRemoveEntry.Size = new System.Drawing.Size(160, 39);
+            this.buttonRemoveEntry.TabIndex = 3;
+            this.buttonRemoveEntry.Text = "Remove day";
+            this.buttonRemoveEntry.UseVisualStyleBackColor = true;
+            this.buttonRemoveEntry.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonAddExercise
+            // 
+            this.buttonAddExercise.Location = new System.Drawing.Point(375, 430);
+            this.buttonAddExercise.Name = "buttonAddExercise";
+            this.buttonAddExercise.Size = new System.Drawing.Size(165, 39);
+            this.buttonAddExercise.TabIndex = 4;
+            this.buttonAddExercise.Text = "Add exercise";
+            this.buttonAddExercise.UseVisualStyleBackColor = true;
+            // 
+            // btnRemoveExercise
+            // 
+            this.btnRemoveExercise.Location = new System.Drawing.Point(560, 430);
+            this.btnRemoveExercise.Name = "btnRemoveExercise";
+            this.btnRemoveExercise.Size = new System.Drawing.Size(165, 39);
+            this.btnRemoveExercise.TabIndex = 5;
+            this.btnRemoveExercise.Text = "Remove exercise";
+            this.btnRemoveExercise.UseVisualStyleBackColor = true;
+            // 
+            // buttonMarkCompleted
+            // 
+            this.buttonMarkCompleted.Location = new System.Drawing.Point(745, 430);
+            this.buttonMarkCompleted.Name = "buttonMarkCompleted";
+            this.buttonMarkCompleted.Size = new System.Drawing.Size(220, 39);
+            this.buttonMarkCompleted.TabIndex = 6;
+            this.buttonMarkCompleted.Text = "Mark day completed";
+            this.buttonMarkCompleted.UseVisualStyleBackColor = true;
+            this.buttonMarkCompleted.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 512);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(788, 37);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // labelOverallProgress
+            // 
+            this.labelOverallProgress.Location = new System.Drawing.Point(806, 521);
+            this.labelOverallProgress.Name = "labelOverallProgress";
+            this.labelOverallProgress.Size = new System.Drawing.Size(154, 35);
+            this.labelOverallProgress.TabIndex = 8;
+            this.labelOverallProgress.Text = "Overall progress: 0%";
+            this.labelOverallProgress.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelVolumeCaption
+            // 
+            this.labelVolumeCaption.AutoSize = true;
+            this.labelVolumeCaption.Font = new System.Drawing.Font("Lucida Calligraphy", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelVolumeCaption.Location = new System.Drawing.Point(5, 588);
+            this.labelVolumeCaption.Name = "labelVolumeCaption";
+            this.labelVolumeCaption.Size = new System.Drawing.Size(234, 27);
+            this.labelVolumeCaption.TabIndex = 9;
+            this.labelVolumeCaption.Text = "Selected day volume:";
+            // 
+            // labelVolumeValue
+            // 
+            this.labelVolumeValue.AutoSize = true;
+            this.labelVolumeValue.Font = new System.Drawing.Font("Lucida Bright", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelVolumeValue.Location = new System.Drawing.Point(255, 588);
+            this.labelVolumeValue.Name = "labelVolumeValue";
+            this.labelVolumeValue.Size = new System.Drawing.Size(26, 25);
+            this.labelVolumeValue.TabIndex = 10;
+            this.labelVolumeValue.Text = "0";
+            // 
+            // buttonShowStats
+            // 
+            this.buttonShowStats.Location = new System.Drawing.Point(823, 583);
+            this.buttonShowStats.Name = "buttonShowStats";
+            this.buttonShowStats.Size = new System.Drawing.Size(125, 39);
+            this.buttonShowStats.TabIndex = 11;
+            this.buttonShowStats.Text = "Show stats";
+            this.buttonShowStats.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1028, 664);
+            this.Controls.Add(this.buttonShowStats);
+            this.Controls.Add(this.labelVolumeValue);
+            this.Controls.Add(this.labelVolumeCaption);
+            this.Controls.Add(this.labelOverallProgress);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.buttonMarkCompleted);
+            this.Controls.Add(this.btnRemoveExercise);
+            this.Controls.Add(this.buttonAddExercise);
+            this.Controls.Add(this.buttonRemoveEntry);
+            this.Controls.Add(this.buttonAddEntry);
+            this.Controls.Add(this.listBoxExercises);
+            this.Controls.Add(this.listBoxEntries);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxEntries;
+        private System.Windows.Forms.ListBox listBoxExercises;
+        private System.Windows.Forms.Button buttonAddEntry;
+        private System.Windows.Forms.Button buttonRemoveEntry;
+        private System.Windows.Forms.Button buttonAddExercise;
+        private System.Windows.Forms.Button btnRemoveExercise;
+        private System.Windows.Forms.Button buttonMarkCompleted;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label labelOverallProgress;
+        private System.Windows.Forms.Label labelVolumeCaption;
+        private System.Windows.Forms.Label labelVolumeValue;
+        private System.Windows.Forms.Button buttonShowStats;
+    }
+}
---
+++ UI/MainForm.cs
@@ -0,0 +1,96 @@
+﻿using Gym_Workout_Diary___Tracker.Domain;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Gym_Workout_Diary___Tracker.UI
+{
+    // ------------------------------------------------------------
+    // Student: Ivan Tenev Ivanov
+    // Faculty Number: F115436
+    // File: MainForm.cs
+    // ------------------------------------------------------------
+
+    /// <summary>
+    /// UI слой на приложението, който съдържа бизнес и обработчиците 
+    /// на събития, а визуалните елементи се описват в Designer файла
+    /// </summary>
+    public partial class MainForm : Form
+    {
+        private readonly WorkoutDiary _diary;
+
+        /// <summary>
+        /// конструктор, който извиква InitializeComponent (генериран от Designer)
+        /// и инициализира домейн обекта
+        /// </summary>
+        public MainForm()
+        {
+            // този метод се намира в MainForm.Designer.cs
+            // и създава всички визуални елементи
+            InitializeComponent();
+
+            // празен дневник
+            _diary = new WorkoutDiary();
+
+            // извиква се event handler-а, когато тренировка е маркирана като завършена
+            _diary.WorkoutCompleted += Diary_WorkoutCompleted;
+
+            // изпълнява се метода MainForm_Load при зареждане на формата
+            this.Load += MainForm_Load;
+        }
+
+        /// <summary>
+        /// изпълнява се автоматично веднъж, когато формата се зареди
+        /// тук по-късно ще добавим примерни записи и ще инициализираме елементите на потребителския интерфейс
+        /// </summary>
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // TODO: методите
+            // CreateSampleEntries();
+            // RefreshEntryList();
+            // UpdateOverallProgress();
+        }
+
+        /// <summary>
+        /// методът, който се извиква автоматично, когато WorkoutDiary
+        /// съобщи чрез събитието WorkoutCompleted, че даден запис е завършен
+        /// </summary>
+        private void Diary_WorkoutCompleted(object sender, WorkoutCompletedEventArgs e)
+        {
+            // засега само излиза log в конзолата
+            // в бъдеше може да показва съобщение или да обновява UI
+            Console.WriteLine("Workout completed: " + e.CompletedEntry.Title);
+        }
+
+        private void MainForm_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
